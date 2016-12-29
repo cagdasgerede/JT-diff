a7f926b38e907663da236b03beb548078e7bec4f
serdarkacka <serdarkacka@gmail.com>
2016-12-29 22:09:23 +0300

    Add util test files

---
+++ jtdiff/util/tests/TestAncestorIterator.java
@@ -0,0 +1,67 @@
+package jtdiff.util.tests;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.junit.Test;
+
+/**
+ * 
+ * Tests <code>ancestorIterator</code> method of <code>Tree</code> class
+ *
+ */
+public class TestAncestorIterator extends TestTree {
+	
+	private Integer[] toArray(ArrayList<Integer> arrayList) {
+		return arrayList.toArray(new Integer[arrayList.size()]);
+	}
+	
+	/**
+	 * Iterate from intermediate or leaf nodes upwards
+	 */
+	@Test
+	public void testSuccess() {
+		
+		ArrayList<Integer> ancestorPreorderPositions = new ArrayList<>();
+		Iterator<Integer> iterator = treeTwo.ancestorIterator(4);
+		while(iterator.hasNext()) {
+			ancestorPreorderPositions.add(iterator.next());
+		}
+		Integer[] expected = new Integer[] {4, 3, 1};
+		assertArrayEquals(expected, toArray(ancestorPreorderPositions));
+		
+		ancestorPreorderPositions = new ArrayList<>();
+		iterator = treeTwo.ancestorIterator(2);
+		while(iterator.hasNext()) {
+			ancestorPreorderPositions.add(iterator.next());
+		}
+		expected = new Integer[] {2, 1};
+		assertArrayEquals(expected, toArray(ancestorPreorderPositions));		
+	}
+	
+	/**
+	 * Iterating from the root upwards
+	 */
+	@Test
+	public void testFromTheRoot() {
+		ArrayList<Integer> ancestorPreorderPositions = new ArrayList<>();
+		Iterator<Integer> iterator = treeTwo.ancestorIterator(1);
+		while(iterator.hasNext()) {
+			ancestorPreorderPositions.add(iterator.next());
+		}
+		Integer[] expected = new Integer[] {1};
+		assertArrayEquals(expected, toArray(ancestorPreorderPositions));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testFromNonExistingPosition() {
+		
+		Iterator<Integer> iterator = treeTwo.ancestorIterator(100);
+		while(iterator.hasNext()) {
+			fail("Should not return anyhting");
+		}
+	}
+}
---
+++ jtdiff/util/tests/TestTree.java
@@ -0,0 +1,80 @@
+package jtdiff.util.tests;
+
+
+
+
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+
+
+import jtdiff.util.Tree;
+import jtdiff.util.TreeNode;
+import jtdiff.util.Visitor;
+
+class TraversalVisitor extends Visitor {
+	private ArrayList<String> traversal = new ArrayList<>();
+	
+	@Override
+	public void visit(TreeNode node) {
+		traversal.add(node.debugString());
+		
+	}
+	
+	public String[] getTraversal() {
+		return traversal.toArray(new String[traversal.size()]);
+	}
+	
+}
+
+
+/**
+ * 
+ * Tests the functionality of Tree
+ *
+ */
+public class TestTree {
+	protected Tree treeOne, treeTwo, treeThree;
+
+	@Before
+	public void setUp() throws Exception {
+		TreeNode aNode = new TreeNode("A");
+		TreeNode bNode = new TreeNode("B");
+		aNode.addChild(bNode);
+		TreeNode cNode = new TreeNode("C");
+		bNode.addChild(cNode);		
+		treeOne = new Tree(aNode);
+		treeOne.buildCaches();
+		
+		
+		aNode = new TreeNode("A");
+		bNode = new TreeNode("B");
+		cNode = new TreeNode("C");
+		TreeNode dNode = new TreeNode("D");
+		aNode.addChild(bNode);
+		aNode.addChild(cNode);
+		cNode.addChild(dNode);
+		treeTwo = new Tree(aNode);
+		treeTwo.buildCaches();
+		
+		aNode = new TreeNode("A");
+		bNode = new TreeNode("B");
+		cNode = new TreeNode("C");
+		dNode = new TreeNode("D");
+		TreeNode eNode = new TreeNode("E");
+		aNode.addChild(bNode);
+		aNode.addChild(cNode);
+		cNode.addChild(dNode);
+		dNode.addChild(eNode);
+		treeThree = new Tree(aNode);
+		treeThree.buildCaches();
+	}
+}
+
+
+
+
+
+
+
