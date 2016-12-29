d76da12ea30f439716db0e2d1c14213e3b35112a
serdarkacka <serdarkacka@gmail.com>
2016-12-29 22:06:31 +0300

    Add util files

---
+++ jtdiff/util/Visitor.java
@@ -0,0 +1,10 @@
+package jtdiff.util;
+
+/**
+ * 
+ * A visitor class to apply the visitor pattern for navigating the tree
+ *
+ */
+abstract public class Visitor {
+	abstract public void visit(TreeNode node);
+}
